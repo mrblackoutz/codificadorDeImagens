607e5193f14b61989e2dfd0be29cafe5e8f713c8
Vinícius Sanches Cappatti <135712810+vinicius-cappatti@users.noreply.github.com>
2023-11-08 19:52:53 -0300

    teste hello world

---
+++ codificadorDeImagens.c
@@ -0,0 +1,11 @@
+/* ****************************************************************************************************************************************
+* Programa: Esse programa contem um programa que busca codificar uma imagem binaria expressa na forma de 0 e 1 em um codigo de letras
+* Autores: Gustavo Nascimento Siqueira & Vinicius Sanches Cappatti;
+* Contato: gustavonascimento.siqueira@mackenzista.com.br & 32342901@mackenzista.com.br
+**************************************************************************************************************************************** */
+#include <stdio.h>
+
+int main(){
+    printf("Hello world!");
+    return 0;
+}
