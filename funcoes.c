6bb8dd640b4560929b11fca15fabe63a7877a358
Vinícius Sanches Cappatti <135712810+vinicius-cappatti@users.noreply.github.com>
2023-11-08 21:35:47 -0300

    create funcoes

---
+++ funcoes.c
@@ -0,0 +1,14 @@
+/* ***************************************************************************************************************************
+* Programa: Esse arquivo contem a definicao de funcoes a serem utilizadas no arquivo principal codificadorDeImagans.c
+* Autores: Gustavo Nascimento Siqueira & Vinicius Sanches Cappatti;
+* Contato: gustavonascimento.siqueira@mackenzista.com.br & 32342901@mackenzista.com.br
+*************************************************************************************************************************** */
+
+#include "funcoes.h"
+#include <stdio.h>
+#include <math.h>
+
+int ehUniforme(int vetor[], int tam, int cor){
+    int i, corBarra = cor;
+    if(tam == )
+}
